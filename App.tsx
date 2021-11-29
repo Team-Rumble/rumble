dc839c858141477e6a77b1ade1af6a92d304bfd2
Indi Rinearson <indi.rinearson@gmail.com>
2021-11-29 09:14:05 -0800

    first connection to db

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { registerRootComponent } from "expo";
+import React from "react";
+import { StyleSheet, Text, View, Button, Alert } from "react-native";
+import db from "./config/firebase";
+import {
+  collection,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+  doc,
+  onSnapshot,
+} from "firebase/firestore";
+
+async function testDB() {
+  const promptsRef = collection(db, "prompts");
+  const prompts = await getDocs(promptsRef);
+  console.log(prompts.docs);
+}
 
 export default function App() {
+  testDB();
   return (
     <View style={styles.container}>
       <Text>Hello friends!</Text>
+      <Button title="Fetch!" onPress={() => Alert.alert(`Fetching...`)} />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +32,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+registerRootComponent(App);
