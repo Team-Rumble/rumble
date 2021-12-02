23ddf07f1f98aaa07a0117db61ade1d513c8a1aa
Indi Rinearson <indi.rinearson@gmail.com>
2021-12-01 20:28:03 -0800

    feat: framework for HomePage, fix: keyboard avoiding view on LogIn

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,29 +1,26 @@
-import React, {FC} from 'react'
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import { createNativeStackNavigator, NativeStackNavigationProp } from '@react-navigation/native-stack';
-import SignIn from '../screens/SignInScreen';
-import SignUpScreen from '../screens/SignUpScreen';
-import HomePageScreen from '../screens/HomePageScreen'
-import { RootStackParamList } from '../../App';
-
-
-
+import React, { FC } from "react";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from "@react-navigation/native-stack";
+import LogIn from "../screens/LogInScreen";
+import SignUpScreen from "../screens/SignUpScreen";
+import HomePageScreen from "../screens/HomePageScreen";
+import { RootStackParamList } from "../../App";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
- interface Props {
-   
- }
- 
- export const NavigationScreens: FC = () => {
-   return (
+interface Props {}
+
+export const NavigationScreens: FC = () => {
+  return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="LogIn" component={LogIn} />
         <Stack.Screen name="SignUp" component={SignUpScreen} />
         <Stack.Screen name="HomePage" component={HomePageScreen} />
       </Stack.Navigator>
     </NavigationContainer>
-   )
- }
- 
+  );
+};
