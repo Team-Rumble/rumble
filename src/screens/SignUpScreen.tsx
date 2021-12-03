90c9fe9f5516e3227113a4e92a6a235438d543c3
Gi Diaz <yandelydf@gmail.com>
2021-12-03 13:07:53 -0500

    (Fix): Age vs Agreement + Username input

--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -1,13 +1,11 @@
 import React, { FC, useState, useEffect } from "react";
 import {
   View,
-  Text,
   Alert,
-  TextInput,
   Image,
   ScrollView,
-  Platform,
-  TouchableOpacity,
+  Platform, Text,
+  TouchableOpacity, Modal
 } from "react-native";
 import styled from "styled-components/native";
 import db, { auth, userRef } from "../../config/firebase";
@@ -20,8 +18,8 @@ import { RumbleBtn, RumbleTxt } from "../components/HomePage.style";
 const images = {
   angryGirl:
     "https://www.news.ucsb.edu/sites/default/files/images/2014/angry%20face.jpg",
-  angryMsn:
-    "http://1.bp.blogspot.com/-1-yZXyA3oY8/VoK8n89SVOI/AAAAAAAAR2Q/tz5kFjjkQDU/s1600/brewing-anger.png",
+  angryBaby:
+    "https://i.pinimg.com/474x/d6/c3/fe/d6c3fef25a327db1b138dbca81d4771b.jpg",
   angryBird:
     "https://d21tktytfo9riy.cloudfront.net/wp-content/uploads/2019/01/23140919/dream_blast_icon.jpg",
 };
@@ -35,9 +33,22 @@ interface UserProps {
   age?: string;
 }
 
+
+// const Agreement: FC = () => {
+//   const [modalVisible, setModalVisible] = useState(true)
+//   return (<Modal
+//     animationType="slide"
+//     visible={modalVisible}
+//     >
+//     <Text>Hello</Text>
+//   </Modal>)
+// }
+
+
 const SignUpScreen: FC<UserProps> = () => {
+  const [username, setUsername] = useState("")
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState(""); // space replace& capital first letter
+  const [password, setPassword] = useState(""); // space replace & capital first letter
   const [age, setAge] = useState("");
   const [bio, setBio] = useState("");
   const [profileUrl, setProfileUrl] = useState(
@@ -56,26 +67,64 @@ const SignUpScreen: FC<UserProps> = () => {
     return unsubscribe;
   }, []);
 
+  // function agreement() {
+  //   Alert.alert(
+  //     "Alert Title",
+  //     "I acknowledge I will not discriminate based on the grounds of race, religion, sexual orientation, political beliefs, age, and gender. If I break this rule, I understand that I will be banned from the app permanently. Rumble is about having a good clean fight.",
+  //     [
+  //       {
+  //         text: "Cancel",
+  //         onPress: () =>
+  //           Alert.alert(
+  //             "If you don't agree, can't Rumble my friend!"
+  //           ),
+  //         style: "cancel",
+  //       },
+  //       { text: "I Accept", onPress: () => true},
+  //     ]
+  //   )
+  // }
+
   async function handleSignUp() {
     try {
       // Replacing spaces in typescript like in JS by using string.replace().
       email.replace(/\s/g, "");
       password.replace(/\s/g, "");
       if (handleAge()) {
-        const userCredential = await auth.createUserWithEmailAndPassword(
-          email,
-          password
-        );
+        Alert.alert(
+          "Alert Title",
+          "I acknowledge I will not discriminate based on the grounds of race, religion, sexual orientation, political beliefs, age, and gender. If I break this rule, I understand that I will be banned from the app permanently. Rumble is about having a good clean fight.",
+          [
+            {
+              text: "Cancel",
+              onPress: () =>
+                Alert.alert(
+                  "If you don't agree, can't Rumble my friend!"
+                ),
+              style: "cancel",
+            },
+            { text: "I Accept", onPress: async () => {
+              const userCredential = await auth.createUserWithEmailAndPassword(
+                email,
+                password
+              );
+      
+              const user = userCredential.user;
+              console.log("Registered with: ", user);
+              await setDoc(doc(db, "users", user.uid), {
+                username: username,
+                email: email,
+                // password: password, // Try to sent user without password
+                age: Number(age), //Sending age as a number type
+                bio: bio,
+                profileUrl: profileUrl,
+              });
+            }},
+          ]
+        )
+        // if(agreement()){
 
-        const user = userCredential.user;
-        console.log("Registered with: ", user);
-        await setDoc(doc(db, "users", user.uid), {
-          email: email,
-          // password: password, // Try to sent user without password
-          age: Number(age), //Sending age as a number type
-          bio: bio,
-          profileUrl: profileUrl,
-        });
+        // }
       }
     } catch (error: any) {
       Alert.alert(error.message);
@@ -99,6 +148,14 @@ const SignUpScreen: FC<UserProps> = () => {
               clearButtonMode="while-editing"
               autoCapitalize="none"
               keyboardType="email-address"
+              placeholder="Username"
+              value={username}
+              onChangeText={(text) => setUsername(text)}
+            ></Input>
+            <Input
+              clearButtonMode="while-editing"
+              autoCapitalize="none"
+              keyboardType="email-address"
               placeholder="Email"
               value={email}
               onChangeText={(text) => setEmail(text)}
@@ -116,7 +173,6 @@ const SignUpScreen: FC<UserProps> = () => {
                 clearButtonMode="while-editing"
                 keyboardType="number-pad"
                 maxLength={2}
-                // textContentType="oneTimeCode"
                 placeholder="Age"
                 value={age}
                 onChangeText={(age) => setAge(age)}
@@ -139,11 +195,11 @@ const SignUpScreen: FC<UserProps> = () => {
                   />
                 </TouchableOpacity>
                 <TouchableOpacity
-                  onPress={() => setProfileUrl(images.angryMsn)}
+                  onPress={() => setProfileUrl(images.angryBaby)}
                 >
                   <Image
                     style={{ width: 80, height: 80, borderRadius: 20 }}
-                    source={{ uri: images.angryMsn }}
+                    source={{ uri: images.angryBaby }}
                   />
                 </TouchableOpacity>
                 <TouchableOpacity
@@ -155,11 +211,6 @@ const SignUpScreen: FC<UserProps> = () => {
                   />
                 </TouchableOpacity>
               </View>
-              {/* <Input
-                placeholder="Enter Image Url"
-                value={profileUrl}
-                onChangeText={(text) => setProfileUrl(text)}
-              /> */}
               <BioInput
                 keyboardType="twitter"
                 multiline={true}
@@ -172,23 +223,7 @@ const SignUpScreen: FC<UserProps> = () => {
             </View>
             <RumbleBtn
               style={{ marginTop: 20, marginBottom: 50 }}
-              onPress={() =>
-                Alert.alert(
-                  "Alert Title",
-                  "I acknowledge I will not discriminate based on the grounds of race, religion, sexual orientation, political beliefs, age, and gender. If I break this rule, I understand that I will be banned from the app permanently. Rumble is about having a good clean fight.",
-                  [
-                    {
-                      text: "Cancel",
-                      onPress: () =>
-                        console.log(
-                          "If you don't agree, can't Rumble my friend!"
-                        ),
-                      style: "cancel",
-                    },
-                    { text: "I Accept", onPress: handleSignUp },
-                  ]
-                )
-              }
+              onPress={handleSignUp}
             >
               <RumbleTxt>Let's Rumble</RumbleTxt>
             </RumbleBtn>
