f6644facebc441bca6cae5825728bd9ddedc80d9
Gi Diaz <yandelydf@gmail.com>
2021-12-04 14:39:52 -0500

    Task: Log Out Button

--- src/screens/UserProfileScreen.tsx
+++ src/screens/UserProfileScreen.tsx
@@ -1,5 +1,8 @@
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import React, { FC, useState, useEffect } from "react";
-import { Text, Alert, ScrollView, Modal, Button, View } from "react-native";
+import { Text, Alert, ScrollView, Modal, Button, View, TouchableOpacity} from "react-native";
+import { auth } from "../../config/firebase";
 import {
   RumbleBtn,
   RumbleTxt,
@@ -7,12 +10,51 @@ import {
   RivalPFP,
   RivalName,
   ClickableRival,
-} from "../components/Stylesheet";
+} from "../components/HomePage.style";
+import { RootStackParamList } from "../navigation";
+
+type profileStack = NativeStackNavigationProp<RootStackParamList, "UserProfile">
+
+
+const dummyUser = {
+  username: "classroom24",
+  age: 27,
+  email: 'test@gmail.com',
+  imageUrl:
+    "https://www.news.ucsb.edu/sites/default/files/images/2014/angry%20face.jpg",
+  bio: "I hate zoom, I hate providing information and I especially hate answering questions. I love being vague and mysterious.",
+};
 
 const UserProfileScreen: FC = () => {
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState(""); // space replace & capital first letter
+  const [age, setAge] = useState("");
+  const [bio, setBio] = useState("");
+  // const [profileUrl, setProfileUrl] = useState(
+  //   "" ||
+  //     "https://www.news.ucsb.edu/sites/default/files/images/2014/angry%20face.jpg"
+  // );
+
+  const navigation = useNavigation<profileStack>();
+
+  async function handleSignOut(){
+    try{
+      const user = await auth.signOut();
+      console.log("User logged out info", user);
+      if(!user) {navigation.navigate("LogIn")}
+      
+    }catch(error: any){
+      Alert.alert(error.message)
+    }
+  }
+
     return(
         <View>
-        <Text>Hi there! I'm a user!</Text>
+        <View></View>
+        <RumbleBtn onPress={handleSignOut}>
+          <RumbleTxt>Log Out</RumbleTxt>
+        </RumbleBtn>
     </View>
     )
 }
